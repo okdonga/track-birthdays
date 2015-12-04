3c78f8d79a2c312d87684604c5ff057c90da55a2
Katie Ok <okdonga@gmail.com>
2015-12-03 20:25:55 -0800

    reindent

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   resources :users
   resources :friends do
-      resources :presents
+    resources :presents
   end
 
   root 'index#index'
