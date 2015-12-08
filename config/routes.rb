b01dbfdb716a730940d48a97fbc6e2cdc19beaf1
Katie Ok <okdonga@gmail.com>
2015-12-07 17:20:49 -0800

    user can log in and sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  root 'index#index'
 
   resources :presents
   # The priority is based upon order of creation: first created -> highest priority.
@@ -21,7 +23,6 @@ Rails.application.routes.draw do
     resources :presents
   end
 
-  root 'index#index'
   # Example resource route with options:
   #   resources :products do
   #     member do
